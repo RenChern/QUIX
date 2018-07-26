dfcfe8f330a695f5aa1012dcc1f7649d1f09498c
RenChern <lincolnchern@gmail.com>
2018-07-26 18:11:41 -0300

    Update quix.sh

--- quix.sh
+++ quix.sh
@@ -27,7 +27,7 @@ wget -q https://github.com/RenChern/QUIX/releases/download/Quix/quix.zip
 unzip quix.zip
 chmod 777 quixd quix-cli quix-tx bench_quix
 
-mv quixd quix-cli quix-tx bench_quix /usr/local/bin
+mv quixd quix-cli quix-tx bench_quix /usr/local/bin/
 mkdir $HOME/.quixcore
 YOURIP=`wget -qO- eth0.me`
 printf "rpcuser=masternode\nrpcpassword=PutHereRandomPassword\ndaemon=1\nlisten=1\nserver=1\nmaxconnections=512\nrpcallowip=127.0.0.1\nexternalip=$YOURIP:6333\nmasternode=1\nmasternodeprivkey=" > /$HOME/.quixcore/quix.conf
